ccb6f5984b83ea774ea74c1794d22fe18b13cbb2
karinemso <karinemsoliveira@gmail.com>
2024-03-22 16:40:27 -0300

    Atualizando imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect } from 'react'
+
+
+import { camera, faceDetectionAdapter, loadFaceDetectorModels } from '@biopassid/face-sdk'
+
  function Validation() {
 
     
